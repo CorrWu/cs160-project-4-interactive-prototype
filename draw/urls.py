b47f29c46369deed4b0eea416b782e8fc98a9a74
Verena Yiu <verenayiu@berkeley.edu>
2021-07-26 03:53:06 -0700

    added phone pages before game starts

--- draw/urls.py
+++ draw/urls.py
@@ -4,7 +4,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('home_phone/', views.home_phone, name='home_phone'),
+    path('instructions_phone/', views.instructions_phone, name='instructions_phone'),
+    path('instructions_phone_slide/', views.instructions_phone_slide, name='instructions_phone_slide'),
+    path('start_phone/', views.start_phone, name='start_phone'),
+    path('join_phone/', views.join_phone, name='join_phone'),
+    path('num_players_phone/', views.num_players_phone, name='num_players_phone'),
+    path('num_teams_phone/', views.num_teams_phone, name='num_teams_phone'),
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,7 +4,28 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name': room_name
-    })
+def home(request):
+    return render(request, 'draw/home.html')
+
+def home_phone(request):
+    return render(request, 'draw/home_phone.html')
+
+def instructions_phone(request):
+    return render(request, 'draw/instructions_phone.html')
+
+def instructions_phone_slide(request):
+    return render(request, 'draw/instructions_phone_slide.html')
+
+def start_phone(request):
+    return render(request, 'draw/start_phone.html')
+
+def join_phone(request):
+    return render(request, 'draw/join_phone.html')
+
+def num_players_phone(request):
+    return render(request, 'draw/num_players_phone.html')
+
+def num_teams_phone(request):
+    return render(request, 'draw/num_teams_phone.html')
+
+
